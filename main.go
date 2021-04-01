346f1b23cd92eef538f631b505f498398d362d37
msk6252 <msk6252@yahoo.co.jp>
2021-04-02 02:05:37 +0900

    add: ランキング情報を格納したハッシュを作成

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-  //"os"
-  "log"
-  "fmt"
+	//"os"
+	//"log"
+	"fmt"
 
-  "github.com/msk6252/BlogRankingAggregate/tools/analytics"
-  //"github.com/msk6252/BlogRankingAggregate/tools/twitter"
+	"github.com/msk6252/BlogRankingAggregate/tools/analytics"
+	//"github.com/msk6252/BlogRankingAggregate/tools/twitter"
 )
 
 func main() {
-  result, err := analytics.GetAnalytics()
-  if err != nil {
-    log.Fatalln("Get Secret Error")
-  }
-  fmt.Println(result)
+  result := analytics.GetAnalytics()
+  hashMap := analytics.CreatePathHashMap(result)
+  orderHashMap := analytics.MaxPVOrderMap(hashMap, 3)
+  fmt.Println(orderHashMap)
 }
--- tools/analytics/analytics.go
+++ tools/analytics/analytics.go
@@ -1,23 +1,25 @@
 package analytics
 
 import (
-  "golang.org/x/oauth2"
-  "golang.org/x/oauth2/google"
-  "log"
-  "github.com/msk6252/BlogRankingAggregate/tools/aws"
-  analytics "google.golang.org/api/analytics/v3"
+	"log"
+	"regexp"
+  "strconv"
+
+	"github.com/msk6252/BlogRankingAggregate/tools/aws"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	analytics "google.golang.org/api/analytics/v3"
 )
 
 const REGION = "ap-northeast-1"
 
-func GetAnalytics() (*analytics.GaData, error) {
+func GetAnalytics() *analytics.GaData {
   key, err := aws.GetSecret("BLOG_RANKING_SECRET", "SECRET_JSON", REGION)
   if err != nil {
     log.Fatalln("GET SECRET ERROR: ", err)
   }
 
   byteKey := ([]byte)(key)
-
   viewId, err := aws.GetSecret("BLOG_RANKING_SECRET", "ANALYTICS_VIEW_ID", REGION)
 
   jwtConf, err := google.JWTConfigFromJSON(
@@ -26,7 +28,7 @@ func GetAnalytics() (*analytics.GaData, error) {
   )
 
   if err != nil {
-    log.Fatalln(err)
+    log.Fatalln("Authenticate Google Auth:", err)
   }
 
   httpClient := jwtConf.Client(oauth2.NoContext)
@@ -39,5 +41,42 @@ func GetAnalytics() (*analytics.GaData, error) {
 
   result, err := svc.Data.Ga.Get("ga:" + viewId, "7daysAgo", "today", "ga:pageviews").Dimensions("ga:pagePath").Filters("ga:pagePath=~^/archives/").Sort("-ga:pageviews").MaxResults(20).Do()
 
-  return result, err
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  return result
+}
+
+func CreatePathHashMap(aryURL *analytics.GaData) map[string]int {
+  rep := regexp.MustCompile(`\?.*|/amp.*`)
+  hashmap := map[string]int{}
+
+  for _, row := range aryURL.Rows {
+    mapKey := rep.ReplaceAllString(row[0], "")
+    pageview, err := strconv.Atoi(row[1])
+    if err != nil {
+      log.Fatalln("STRCONV Error: ", err)
+    }
+    hashmap[mapKey] = hashmap[mapKey] + pageview
+  }
+
+  return hashmap
+}
+
+func MaxPVOrderMap(hashMap map[string]int, rank int) map[int]map[string]int {
+  orderHashMap := map[int]map[string]int{}
+  deleteUrl := ""
+  for i:=1; i<=rank; i++ {
+    max := -1
+    for url, pageview := range hashMap {
+      if max <= pageview {
+        max = pageview
+        deleteUrl = url
+        orderHashMap[i] = map[string]int{url : pageview}
+      }
+    }
+    delete(hashMap,deleteUrl)
+  }
+  return orderHashMap
 }
